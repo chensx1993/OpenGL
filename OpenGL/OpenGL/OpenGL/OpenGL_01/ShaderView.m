1b3f2c49ebd38ceb9c79c033a9ae59643e4ba98c
chensx <844711603@qq.com>
2018-05-22 18:44:12 +0800

    feat: shader实现

--- OpenGL/OpenGL/OpenGL/OpenGL_01/ShaderView.m
+++ OpenGL/OpenGL/OpenGL/OpenGL_01/ShaderView.m
@@ -15,17 +15,167 @@
 @property (nonatomic, strong) CAEAGLLayer *myEALayer;
 @property (nonatomic, assign) GLuint myProgram;
 
+@property (nonatomic, assign) GLuint myColorRenderBuffer;
+@property (nonatomic, assign) GLuint myColorFrameBuffer;
 
 @end
 
 @implementation ShaderView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
++ (Class)layerClass {
+    return [CAEAGLLayer class];
 }
-*/
+
+- (void)layoutSubviews {
+    
+    [self setupLayer];
+    
+    [self setupContext];
+    
+    [self destoryRenderAndFrameBuffer];
+    
+    [self setupRenderBuffer];
+    
+    [self setupFrameBuffer];
+    
+    [self render];
+}
+
+#pragma mark - 渲染
+- (void)render {
+    glClearColor(0, 1.0, 0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    
+    CGFloat scale = [[UIScreen mainScreen] scale]; //获取视图放大倍数，可以把scale设置为1试试
+    glViewport(self.frame.origin.x * scale, self.frame.origin.y * scale, self.frame.size.width * scale, self.frame.size.height * scale); //设置视口大小
+    
+    //读取文件路径
+    NSString* vertFile = [[NSBundle mainBundle] pathForResource:@"shaderv" ofType:@"vsh"];
+    NSString* fragFile = [[NSBundle mainBundle] pathForResource:@"shaderf" ofType:@"fsh"];
+    
+    //加载shader
+    self.myProgram = [self loadShaders:vertFile frag:fragFile];
+    
+    //链接
+    glLinkProgram(self.myProgram);
+    GLint linkSuccess;
+    glGetProgramiv(self.myProgram, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetProgramInfoLog(self.myProgram, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"error%@", messageString);
+        return ;
+    }
+    else {
+        NSLog(@"link ok");
+        glUseProgram(self.myProgram); //成功便使用，避免由于未使用导致的的bug
+    }
+    
+    //前三个是顶点坐标， 后面两个是纹理坐标
+    GLfloat attrArr[] =
+    {
+        0.5f, -0.5f, -1.0f,     1.0f, 0.0f,
+        -0.5f, 0.5f, -1.0f,     0.0f, 1.0f,
+        -0.5f, -0.5f, -1.0f,    0.0f, 0.0f,
+        0.5f, 0.5f, -1.0f,      1.0f, 1.0f,
+        -0.5f, 0.5f, -1.0f,     0.0f, 1.0f,
+        0.5f, -0.5f, -1.0f,     1.0f, 0.0f,
+    };
+    
+    GLuint attrBuffer;
+    glGenBuffers(1, &attrBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, attrBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(attrArr), attrArr, GL_DYNAMIC_DRAW);
+    
+}
+
+- (GLuint)loadShaders:(NSString *)vert frag:(NSString *)frag {
+    GLuint verShader, fragShader;
+    GLint program = glCreateProgram();
+    
+    //编译
+    [self compileShader:&verShader type:GL_VERTEX_SHADER file:vert];
+    [self compileShader:&fragShader type:GL_FRAGMENT_SHADER file:frag];
+    
+    glAttachShader(program, verShader);
+    glAttachShader(program, fragShader);
+    
+    //释放不需要的shader
+    glDeleteShader(verShader);
+    glDeleteShader(fragShader);
+    
+    return program;
+}
+
+- (void)compileShader:(GLuint *)shader type:(GLenum)type file:(NSString *)file {
+    NSString *content = [NSString stringWithContentsOfFile:file encoding:NSUTF8StringEncoding error:nil];
+    const GLchar *source = (GLchar *)[content UTF8String];
+    
+    *shader = glCreateShader(type);
+    glShaderSource(*shader, 1, &source, NULL);
+    glCompileShader(*shader);
+}
+
+#pragma mark -
+- (void)setupLayer {
+    self.myEALayer = (CAEAGLLayer *)self.layer;
+    
+    //设置放大倍数
+    [self setContentScaleFactor:[[UIScreen mainScreen] scale]];
+    
+    // CALayer 默认是透明的，必须将它设为不透明才能让其可见
+    self.myEALayer.opaque = YES;
+    self.myEALayer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys:
+                                          @(NO), kEAGLDrawablePropertyRetainedBacking, kEAGLColorFormatRGBA8, kEAGLDrawablePropertyColorFormat, nil];
+}
+
+- (void)setupContext {
+    // 指定 OpenGL 渲染 API 的版本，在这里我们使用 OpenGL ES 2.0
+    EAGLRenderingAPI api = kEAGLRenderingAPIOpenGLES2;
+    EAGLContext *context = [[EAGLContext alloc] initWithAPI:api];
+    if (!context) {
+        NSLog(@"Failed to initialize OpenGLES 2.0 context");
+        exit(1);
+    }
+    
+    // 设置为当前上下文
+    if (![EAGLContext setCurrentContext:context]) {
+        NSLog(@"Failed to set current OpenGL context");
+        exit(1);
+    }
+    self.myContext = context;
+}
+
+- (void)setupRenderBuffer {
+    GLuint buffer;
+    glGenRenderbuffers(1, &buffer);
+    
+    self.myColorRenderBuffer = buffer;
+    glBindRenderbuffer(GL_RENDERBUFFER, self.myColorRenderBuffer);
+    
+    // 为 颜色缓冲区 分配存储空间
+    [self.myContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:self.myEALayer];
+    
+}
+
+- (void)setupFrameBuffer {
+    GLuint buffer;
+    glGenFramebuffers(1, &buffer);
+    self.myColorFrameBuffer = buffer;
+    // 设置为当前 framebuffer
+    glBindFramebuffer(GL_FRAMEBUFFER, self.myColorFrameBuffer);
+    // 将 _colorRenderBuffer 装配到 GL_COLOR_ATTACHMENT0 这个装配点上
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+                              GL_RENDERBUFFER, self.myColorRenderBuffer);
+}
+
+- (void)destoryRenderAndFrameBuffer {
+    glDeleteFramebuffers(1, &_myColorFrameBuffer);
+    self.myColorFrameBuffer = 0;
+    glDeleteRenderbuffers(1, &_myColorRenderBuffer);
+    self.myColorRenderBuffer = 0;
+}
 
 @end
