deb8d87562f2d17bac5efba4b4951b770d69c8d3
chensx1993 <844711603@qq.com>
2019-03-21 14:44:50 +0800

    feat: 添加项目光照 跟 摄像机

---
+++ LearnOpenGL/第2章_光照/2.1.颜色/LearnOpenGL/LearnOpenGL/Common/ESUtil/FileWrapper.cpp
@@ -0,0 +1,23 @@
+//
+//  FileWrapper.c
+//  LearnOpenGL
+//
+//  Created by 陈思欣 on 2019/2/28.
+//  Copyright © 2019 chensx. All rights reserved.
+//
+
+#include "FileWrapper.hpp"
+#import <CoreFoundation/CoreFoundation.h>
+
+const char *GetBundleFileName( const char *fileName)
+{
+    CFStringRef name = CFStringCreateWithCString(NULL, fileName, kCFStringEncodingUTF8);
+    CFURLRef fileURL = CFBundleCopyResourceURL(CFBundleGetMainBundle(), name, NULL, NULL);//CFSTR("")
+    if (fileURL) {
+        CFStringRef newfilePath = CFURLCopyFileSystemPath(fileURL, kCFURLPOSIXPathStyle);
+        CFStringEncoding encodingMethod = CFStringGetSystemEncoding();
+        const char *path = CFStringGetCStringPtr(newfilePath, encodingMethod);
+        return path;
+    }
+    return fileName;
+}
